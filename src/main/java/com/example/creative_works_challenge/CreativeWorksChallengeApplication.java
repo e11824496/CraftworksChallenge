4c2cac612b51b7c9811b68fb14c32a691675c875
Fabian <e11824496@student.tuwien.ac.at>
2023-08-02 08:58:21 +0200

    initialize spring boot project

---
+++ src/main/java/com/example/creative_works_challenge/CreativeWorksChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.creative_works_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeWorksChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeWorksChallengeApplication.class, args);
+	}
+
+}
